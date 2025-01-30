0d381391ef3de7cc09b35c99709554cbeeb53369
Lyrecoph <lyrecoph30@gmail.com>
2025-01-30 17:52:23 +0100

    Installation de quelques librairies et implémentation de la gestion des factures avec Next.js et Prisma

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,25 +1,143 @@
 'use server';
 
-import prisma from "../../lib/prisma";
+import { randomBytes } from "crypto";
+import prisma from "../../lib/prisma"; // Importation du client Prisma pour interagir avec la base de données
 
+/**
+ * Vérifie si un utilisateur existe par son email. 
+ * Si l'utilisateur n'existe pas et qu'un nom est fourni, il est ajouté à la base de données.
+ * @param {string} email - Email de l'utilisateur.
+ * @param {string} name - Nom de l'utilisateur.
+ */
 export async function checkAndAddUser(email: string, name: string) {
-    if(!email) return;
+    if (!email) return; // Empêche l'exécution si l'email est vide
 
     try {
+        // Recherche de l'utilisateur par email
         const existingUser = await prisma.user.findUnique({
-            where: {
-                email: email
-            }
-        })
-        if(!existingUser && name){
+            where: { email }
+        });
+
+        // Si l'utilisateur n'existe pas et que le nom est fourni, création d'un nouvel utilisateur
+        if (!existingUser && name) {
             await prisma.user.create({
-                data: {
-                    email,
-                    name
-                }
-            })
+                data: { email, name }
+            });
         }
     } catch (error) {
-        console.log(error)
+        console.log("Erreur lors de l'ajout de l'utilisateur:", error);
+    }
+}
+
+/**
+ * Génère un identifiant unique pour une facture en vérifiant qu'il n'est pas déjà utilisé.
+ * @returns {Promise<string>} - Un identifiant unique sous forme hexadécimale.
+ */
+const generateUniqueId = async (): Promise<string> => {
+    let uniqueId: string = "";
+    let isUnique = false;
+
+    while (!isUnique) {
+        uniqueId = randomBytes(3).toString('hex'); // Génère un identifiant hexadécimal aléatoire
+        
+        // Vérifie si l'identifiant existe déjà dans la base de données
+        const existingInvoiceId = await prisma.invoice.findUnique({
+            where: { id: uniqueId }
+        });
+
+        if (!existingInvoiceId) {
+            isUnique = true; // Si l'ID n'existe pas, il est considéré comme unique
+        }
+    }
+    return uniqueId;
+};
+
+/**
+ * Crée une facture vide pour un utilisateur donné par son email.
+ * @param {string} email - Email de l'utilisateur.
+ * @param {string} name - Nom associé à la facture.
+ */
+export async function createEmptyInvoice(email: string, name: string) {
+    if (!email) return; // Vérifie que l'email est fourni
+    
+    try {
+        // Recherche l'utilisateur correspondant à l'email
+        const user = await prisma.user.findUnique({
+            where: { email }
+        });
+        
+        const invoiceId = await generateUniqueId(); // Génère un identifiant unique pour la facture
+
+        if (user) {
+            // Crée une nouvelle facture associée à l'utilisateur
+            await prisma.invoice.create({
+                data: {
+                    id: invoiceId,
+                    name,
+                    userId: user.id,
+                    issuerAddress: '',
+                    issuerName: '',
+                    clientAddress: '',
+                    clientName: '',
+                    invoiceDate: '',
+                    dueDate: '',
+                    vatActive: false,
+                    vatRate: 20 // TVA par défaut de 20%
+                }
+            });
+        }
+    } catch (error) {
+        console.log("Erreur lors de la création de la facture:", error);
+    }
+}
+
+/**
+ * Récupère les factures associées à un utilisateur en fonction de son email.
+ * Met également à jour le statut des factures expirées.
+ * @param {string} email - Email de l'utilisateur.
+ * @returns {Promise<any[]>} - Liste des factures mises à jour.
+ */
+export async function getInvoiceByEmail(email: string) {
+    try {
+        // Recherche l'utilisateur et ses factures associées
+        const user = await prisma.user.findUnique({
+            where: { email },
+            include: {
+                invoices: {
+                    include: { lines: true } // Inclut les lignes de facturation
+                }
+            }
+        });
+
+        // Statuts possibles des factures
+        // 1: Brouillon
+        // 2: En attente
+        // 3: Payée 
+        // 4: Annulée
+        // 5: Impayée
+        if (user) {
+            const today = new Date();
+            
+            // Vérifie et met à jour les factures en retard
+            const updatedInvoices = await Promise.all(
+                user.invoices.map(async (invoice) => {
+                    const dueDate = new Date(invoice.dueDate);
+                    
+                    // Si la facture est en attente et que la date d'échéance est dépassée, on la marque comme impayée
+                    if (dueDate < today && invoice.status === 2) {
+                        const updatedInvoice = await prisma.invoice.update({
+                            where: { id: invoice.id },
+                            data: { status: 5 }, // Statut: Impayé
+                            include: { lines: true }
+                        });
+                        return updatedInvoice;
+                    }
+                    return invoice;
+                })
+            );
+            return updatedInvoices;
+        }
+    } catch (error) {
+        console.log("Erreur lors de la récupération des factures:", error);
     }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,151 @@
 'use client';
 
+import { useCallback, useEffect, useState } from "react";
 import { redirect } from "next/navigation";
-import { useAuth } from "@clerk/nextjs";
+import { Layers } from "lucide-react";
+import { useAuth, useUser } from "@clerk/nextjs";
+import confetti from "canvas-confetti";
 import Wrapper from "./components/Wrapper";
-export default function Home() {
-  const { userId } = useAuth();
+import { createEmptyInvoice, getInvoiceByEmail } from "./actions";
+import { Invoice } from "../../type";
+import InvoiceComponent from "./components/InvoiceComponent";
 
+export default function Home() {
+  // Récupération de l'identifiant utilisateur et des informations utilisateur
+  const { userId } = useAuth();
+  const user = useUser();
+  
+  // États pour gérer le nom de la facture et la validation de la longueur du nom
+  const [invoiceName, setInvoiceName] = useState("");
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [invoices, setInvoices] = useState<Invoice[]>([]);
+  
+  // Récupération de l'email principal de l'utilisateur connecté
+  const email = user.user?.primaryEmailAddress?.emailAddress ?? "";
+
+  // Fonction pour récupérer les factures associées à l'email de l'utilisateur
+  const fetchInvoices = useCallback(async () =>  {
+    try {
+      const data = await getInvoiceByEmail(email);
+      if(data) setInvoices(data);
+    } catch (error) {
+      console.error("Erreur du chargement lors de la facture", error);
+    }
+  }, [email]);
+
+  // Chargement des factures à chaque changement d'email
+  useEffect(() => {
+    fetchInvoices();
+  }, [fetchInvoices]);
+
+  // Vérification et mise à jour de la validation du nom de la facture
+  useEffect(() => {
+    setIsNameValid(invoiceName.length <= 30);
+  }, [invoiceName]);
+
+  // Affichage d'un écran de chargement tant que les informations utilisateur ne sont pas chargées
+  if (!user.isLoaded) {
+    return (
+      <div className="flex justify-center items-center w-full h-screen">
+        <div className="flex items-center space-x-2">
+          <span className="loading loading-dots loading-xs text-accent"></span>
+          <span className="loading loading-dots loading-sm text-accent"></span>
+          <span className="loading loading-dots loading-md text-accent"></span>
+          <span className="loading loading-dots loading-lg text-accent"></span>
+        </div>
+      </div>
+    );
+  }
+  
+  // Redirection vers la page de connexion si l'utilisateur n'est pas connecté
   if (!userId) {
     redirect("/sign-in");
   }
+
+  // Fonction pour créer une nouvelle facture
+  const handleCreateInvoice = async () => {
+    try {
+      if (email) {
+        await createEmptyInvoice(email, invoiceName);
+      }
+      fetchInvoices(); // Rafraîchir la liste des factures après création
+      setInvoiceName(""); // Réinitialiser le champ de saisie
+      
+      // Fermer la modal de création de facture
+      const modal = document.getElementById('my_modal_3') as HTMLDialogElement;
+      if (modal) {
+        modal.close();
+      }
+
+      // Effet de confettis pour indiquer la création réussie de la facture
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 },
+        zIndex: 9999
+      });
+    } catch (error) {
+      console.error("Erreur lors de la création de la facture:", error);
+    }
+  };
+
   return (
     <Wrapper>
-      <button className="btn btn-success">Send</button>
+      <div className="flex flex-col space-y-4">
+        <h1 className="text-lg font-bold">Mes factures</h1>
+        
+        {/* Grille contenant les factures et le bouton de création */}
+        <div className="grid md:grid-cols-3 gap-4">
+          {/* Bouton pour ouvrir la modal de création de facture */}
+          <div
+            className="cursor-pointer border border-accent rounded-xl flex items-center justify-center"
+            onClick={() => (document.getElementById('my_modal_3') as HTMLDialogElement).showModal()}
+          >
+            <div className="font-bold text-accent m-2">Créer une facture</div>
+            <div className='bg-accent-content text-accent rounded-full p-2 m-2'>
+              <Layers className='h-6 w-6'/>
+            </div>
+          </div>
+          
+          {/* Affichage des factures existantes */}
+          {invoices.length > 0 && (
+            invoices.map((invoice, index) => (
+              <div key={index}>
+                <InvoiceComponent index={index} invoice={invoice} />
+              </div>
+            ))
+          )}
+        </div>
+
+        {/* Modal pour créer une nouvelle facture */}
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            </form>
+            <h3 className="font-bold text-lg">Nouvelle Facture</h3>
+            
+            {/* Champ de saisie du nom de la facture */}
+            <input
+              type="text"
+              placeholder="Nom de la facture (max 30 caractères)"
+              className="input input-bordered w-full my-4"
+              value={invoiceName}
+              onChange={(e) => setInvoiceName(e.target.value)}
+            />
+            {!isNameValid && <p className="mb-4 text-sm text-red-600">Le nom de la facture ne doit pas dépasser 30 caractères</p>}
+            
+            {/* Bouton pour créer la facture */}
+            <button
+              className="btn btn-accent"
+              disabled={!isNameValid || invoiceName.length === 0}
+              onClick={handleCreateInvoice}
+            >
+              Créer
+            </button>
+          </div>
+        </dialog>
+      </div>
     </Wrapper>
   );
 }
