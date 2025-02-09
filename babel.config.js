45771b095f97e713aee8c9b29c7fab5ad447f91f
Lyrecoph <lyrecoph30@gmail.com>
2025-02-10 00:24:46 +0100

    remove babel

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-// babel.config.js
-module.exports = {
-    presets: [
-      '@babel/preset-env',         // Pour transformer le JavaScript moderne
-      '@babel/preset-react',       // Pour transformer le JSX
-      '@babel/preset-typescript'   // Pour gérer les fichiers TypeScript
-    ]
-  };
-  
